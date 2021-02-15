5f1683289a9ae58eda456bd35c047e4c4537db53
Gintare Auks <gintareauks@gmail.com>
2021-02-15 16:34:41 +0200

    Mostly done

---
+++ app.js
@@ -0,0 +1,22 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 2000 + 100 * i
+  })
+//   .add({
+//     targets: '.ml12 .letter',
+//     translateX: [0,-30],
+//     opacity: [1,0],
+//     easing: "easeInExpo",
+//     duration: 2100,
+//     delay: (el, i) => 100 + 30 * i,
+//   });
--- project-5/README.md
+++
@@ -1,2 +0,0 @@
-# online_registration_v4
- 3rd techdegree project

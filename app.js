8267d8a3305b4aff2ece1951587399e359b771aa
Gintare Auks <gintareauks@gmail.com>
2021-04-01 20:45:22 +0300

    ready to publish

--- app.js
+++ app.js
@@ -74,105 +74,95 @@ AOS.init({
 // Projects data
 const data = [
     {
-      id: 0,
-      title: "Responsive Portfolio",
-      image: "personal_projects/project_screenshots/project_1.png",
-      description:
-        "Recreated a portfolio on a provided mockup that look and function well on multiple screen sizes.",
-      technologies: "HTML, CSS",
+        id: 0,
+        title: "Responsive Portfolio",
+        image: "personal_projects/project_screenshots/project_1.png",
+        description: "Recreated a portfolio on a provided mockup that look and function well on multiple screen sizes.",
+        technologies: "HTML, CSS",
         live: "personal_projects/project_1/index.html",
-      github: "https://github.com/gintareauks/Responsive-Layout-Project?",
+        github: "https://github.com/gintareauks/Responsive-Layout-Project?"
     },
     {
-      id: 1,
-      title: "Web App Dashboard",
-      image: "personal_projects/project_screenshots/project_2.png",
-      description:
-        "This interactive WebApp Dashboard provides a range of traffic-related data to the end-user. The app uses chart.js to display bar, line and pie charts. The site is optimised to provide a top-end user experience across mobile, tablet and desktop platforms. As well as displaying traffic informaton via charts, the app also displays widgets that showcase alerts, the latest user registrations, and comments in a forum. Lastly, the app makes use of localstorage to store the user's preferences for their next visit.",
-      technologies: "HTML, CSS/SASS, JavaScript",
-      live: "personal_projects/project_2/index.html",
-      github: "https://github.com/gintareauks/web_app_dashboard_v3?",
+        id: 1,
+        title: "Web App Dashboard",
+        image: "personal_projects/project_screenshots/project_2.png",
+        description: "This interactive WebApp Dashboard provides a range of traffic-related data to the end-user. The app uses chart.js to display bar, line and pie charts. The site is optimised to provide a top-end user experience across mobile, tablet and desktop platforms. As well as displaying traffic informaton via charts, the app also displays widgets that showcase alerts, the latest user registrations, and comments in a forum. Lastly, the app makes use of localstorage to store the user's preferences for their next visit.",
+        technologies: "HTML, CSS/SASS, JavaScript",
+        live: "personal_projects/project_2/index.html",
+        github: "https://github.com/gintareauks/web_app_dashboard_v3?"
     },
     {
-      id: 2,
-      title: "Interactive Photo Gallery",
-      image: "personal_projects/project_screenshots/project_3.png",
-      description:
-        "Interactive, searchable gallery of photos.",
-        technologies: "HTML, CSS, JavaScript",
-      live: "personal_projects/project_3/index.html",
-      github: "https://github.com/gintareauks/photo_gallery_v5?",
+        id: 2,
+        title: "Employee Directory",
+        image: "personal_projects/project_screenshots/project_3.png",
+        description: "Used fetch to request 12 random users from the API. New random employee information displays each time the page refreshes. The directory includes the following: Employee image, first and last name, email and city. Modal window displays the following details: Employee image, name, email, phone number, detailed address and birthday. There is a way to close the modal window. Directory has been styled so that all the major elements are in place. Employees can be filtered by name or username. Functionality has been added to switch back and forth between employees when the detail modal is open.",
+        technologies: "HTML, CSS, JavaScript, AJAX",
+        live: "personal_projects/project_3/index.html",
+        github: "https://github.com/gintareauks/photo_gallery_v5?"
     },
     {
-      id: 3,
-      title: "Game Show App",
-      image: "personal_projects/project_screenshots/project_4.png",
-      description:
-        "A word guessing game. Used JavaScript to come up with a random phrase that players will try to guess by entering different letters into the program.",
-      technologies: "JavaScript",
+        id: 3,
+        title: "Game Show App",
+        image: "personal_projects/project_screenshots/project_4.png",
+        description: "I created a word guessing game using JavaScript and OOP. A random phrase comes up that players will try to guess by entering different letters into the program. They can miss a letter no more than 5 times to win the game.",
+        technologies: "JavaScript, Object-Oriented Programming",
         live: "personal_projects/project_4/index.html",
-      github: "https://github.com/gintareauks/oop_game-v2",
+        github: "https://github.com/gintareauks/oop_game-v2"
     },
     {
         id: 4,
         title: "Responsive Web Form",
         image: "personal_projects/project_screenshots/project_5.png",
-        description:
-          "A responsive registration form.",
+        description: "A responsive registration form that shows my layout skills.",
         technologies: "HTML, CSS",
-          live: "personal_projects/project_5/index.html",
-        github: "https://github.com/gintareauks/online_registration_v4?",
-      },
-      {
+        live: "personal_projects/project_5/index.html",
+        github: "https://github.com/gintareauks/online_registration_v4?"
+    },
+    {
         id: 5,
         title: "Responsive Style Guide",
         image: "personal_projects/project_screenshots/project_6.png",
-        description:
-          "..",
-        technologies: "CSS/SASS",
-          live: "personal_projects/project_6/index.html",
-        github: "https://github.com/gintareauks/style_guide_v7-2?",
-      },
-      {
+        description: "Used Sass to add responsive mobile first styles to the supplied index.html file so that it matches the provided mockups when loaded in a browser.",
+        technologies: "SASS",
+        live: "personal_projects/project_6/index.html",
+        github: "https://github.com/gintareauks/style_guide_v7-2?"
+    },
+    {
         id: 6,
-        title: "Employee Directory",
+        title: "Interactive Photo Gallery",
         image: "personal_projects/project_screenshots/project_7.png",
-        description:
-          "Pulls in data from random user generator API and displays their contact info.",
-        technologies: "HTML, CSS, JavaScript, AJAX",
-          live: "personal_projects/project_7/index.html",
-        github: "https://github.com/gintareauks/employee_directory_v1?",
-      },
-      {
+        description: "Interactive, searchable gallery of photos made using JavaScript and CSS Grid Layout. At the top of the page, I created a search area where photos hide and show depending on user input. When the user clicks on a thumbnail, the photo will display in a lightbox. I also created 'next' and 'previous' arrows to cycle through photos.",
+        technologies: "HTML, CSS, JavaScript, Lightbox",
+        live: "personal_projects/project_7/index.html",
+        github: "https://github.com/gintareauks/employee_directory_v1?"
+    },
+    {
         id: 7,
         title: "Random Quote Generator",
         image: "personal_projects/project_screenshots/project_8.png",
-        description:
-          "..",
+        description: "Created an app that displays random famous quotes each time a button is clicked. Used JavaScript syntax, variables, loops, conditionals and object literals to build the array of quote objects to store the quotes and wrote my own functions for selecting random quotes from the array and printing them to the screen.",
         technologies: "JavaScript",
-          live: "personal_projects/project_8/index.html",
-        github: "https://github.com/gintareauks/a_random_quote_generator-v1?",
-      },
-      {
+        live: "personal_projects/project_8/index.html",
+        github: "https://github.com/gintareauks/a_random_quote_generator-v1?"
+    },
+    {
         id: 8,
         title: "Data Pagination and Filtering",
         image: "personal_projects/project_screenshots/project_9.png",
-        description:
-          "..",
+        description: "Displaying a large amount of data on a web page, like a list of 200 movie titles or 400 products, can overwhelm a user with too much information at once. A common solution is to create separate 'pages' that each display a small portion of the data. In this project, I wrote JavaScript to display several 'pages' of student data that the user can easily navigate and view. Besides pagination, I also added functional search bar. The student data is filtered so that only students whose name includes the search value are shown.",
         technologies: "JavaScript",
-          live: "personal_projects/project_9/index.html",
-        github: "https://github.com/gintareauks/data-pagination-and-filtering-v1?",
-      },
-      {
+        live: "personal_projects/project_9/index.html",
+        github: "https://github.com/gintareauks/data-pagination-and-filtering-v1?"
+    },
+    {
         id: 10,
         title: "Interactive Form",
         image: "personal_projects/project_screenshots/project_10.png",
-        description:
-          "..",
+        description: "Used JavaScript to enhance an interactive registration form for a fictional Full Stack conference. Using the supplied HTML and CSS files, I added my own JavaScript to make the form more user-friendly by: Adding customized and conditional behavior and interactivity; Validating user input and providing helpful error messages when the user enters invalid information into the form fields.",
         technologies: "JavaScript",
-          live: "personal_projects/project_10/index.html",
-        github: "https://github.com/gintareauks/interactive-form-v3?",
-      },
+        live: "personal_projects/project_10/index.html",
+        github: "https://github.com/gintareauks/interactive-form-v3?"
+    }
     ];
 
 const modalContainer = document.querySelector(".overlay");
